774ba29340694c9d364c2c31b2d8cd810e8607fc
Mohammad Marzan <38738228+marzan-666@users.noreply.github.com>
2020-10-19 05:48:38 +0600

    Add files via upload

---
+++ Classes and Objects/Classes&Objects.py
@@ -0,0 +1,8 @@
+from Student import Student
+
+Student1 = Student("Jim", "BBA", 3.4, False)
+
+print(Student1.name)
+print(Student1.major)
+print(Student1.cgpa)
+print(Student1.is_on_probation)
---
+++ Classes and Objects/Student.py
@@ -0,0 +1,7 @@
+class Student:
+
+   def __init__(self, name, major, cgpa, is_on_probation):
+       self.name = name
+       self.major = major
+       self.cgpa = cgpa
+       self.is_on_probation = is_on_probation
